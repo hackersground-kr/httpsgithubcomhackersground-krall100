123084c5b53508a01ceea642713c226adaad3857
kimye702 <kimye702@naver.com>
2023-06-23 06:20:11 +0900

    ㅏㅇ

--- all100/app/src/main/java/com/example/all100/Sttpractice.java
+++ all100/app/src/main/java/com/example/all100/Sttpractice.java
@@ -115,6 +115,7 @@ public class Sttpractice extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == REQUEST_PERMISSION_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 startSpeechToText();
