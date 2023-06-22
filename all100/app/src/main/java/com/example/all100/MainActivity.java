3e727ab761b4c17a5428fef3c8598e329cf991bc
kimye702 <kimye702@naver.com>
2023-06-22 21:53:09 +0900

    mainactivity에 웹앱이랑 안스 연결하는 내용 있음

--- all100/app/src/main/java/com/example/all100/MainActivity.java
+++ all100/app/src/main/java/com/example/all100/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.all100;
 
-
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,19 +8,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import androidx.appcompat.app.AppCompatActivity;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String DB_URL = "jdbc:mysql://your-database-url:3306/your-database-name";
-    private static final String DB_USER = "your-username";
-    private static final String DB_PASSWORD = "your-password";
+    private static final String API_URL = "https://all100.azurewebsites.net"; // Azure 웹 앱의 URL로 변경해주세요
+    private static final String SAVE_DATA_ENDPOINT = "/saveData"; // 저장 엔드포인트 경로로 변경해주세요
 
     private EditText etSql;
     private TextView tvSql;
@@ -29,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.dotest);
 
         etSql = findViewById(R.id.sql_et);
@@ -41,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String name = etSql.getText().toString();
 
-                // AsyncTask를 통해 데이터베이스에 값을 저장하는 작업을 실행합니다.
+                // AsyncTask를 통해 서버로 데이터 저장 요청을 보냅니다.
                 SaveDataAsyncTask saveTask = new SaveDataAsyncTask();
                 saveTask.execute(name);
             }
@@ -51,84 +54,102 @@ public class MainActivity extends AppCompatActivity {
         loadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // AsyncTask를 통해 데이터베이스에서 값을 불러오는 작업을 실행합니다.
+                // AsyncTask를 통해 서버로부터 데이터 불러오기 요청을 보냅니다.
                 LoadDataAsyncTask loadTask = new LoadDataAsyncTask();
                 loadTask.execute();
             }
         });
     }
 
-    private class SaveDataAsyncTask extends AsyncTask<String, Void, Boolean> {
+    private class SaveDataAsyncTask extends AsyncTask<String, Void, String> {
         @Override
-        protected Boolean doInBackground(String... params) {
+        protected String doInBackground(String... params) {
             String name = params[0];
-            boolean success = false;
+            String result = "";
 
             try {
-                Class.forName("com.mysql.jdbc.Driver");
-                Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-                PreparedStatement statement = connection.prepareStatement("INSERT INTO your-table-name (name) VALUES (?)");
-                statement.setString(1, name);
-                int rowsAffected = statement.executeUpdate();
+                // 서버로 데이터를 전송하기 위한 JSON 객체 생성
+                JSONObject jsonParam = new JSONObject();
+                jsonParam.put("name", name);
 
-                success = (rowsAffected > 0);
+                // 서버에 POST 요청을 보냄
+                URL url = new URL(API_URL + SAVE_DATA_ENDPOINT + "?table=test");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setDoOutput(true);
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type", "application/json");
+                OutputStream outputStream = conn.getOutputStream();
+                outputStream.write(jsonParam.toString().getBytes());
+                outputStream.flush();
+                outputStream.close();
 
-                statement.close();
-                connection.close();
-            } catch (SQLException e) {
-                Log.e("SaveDataAsyncTask", "Error executing SQL query: " + e.getMessage());
+                // 응답 코드 확인
+                int responseCode = conn.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    // 요청이 성공적으로 처리됨
+                    result = "Data saved successfully";
+                } else {
+                    // 요청이 실패함
+                    result = "Error saving data: " + responseCode;
+                }
+
+                conn.disconnect();
+            } catch (JSONException | IOException e) {
                 e.printStackTrace();
-            } catch (ClassNotFoundException e) {
-                Log.e("SaveDataAsyncTask", "Error loading JDBC driver: " + e.getMessage());
-                e.printStackTrace();
+                result = "Error saving data: " + e.getMessage();
             }
 
-            return success;
+            return result;
         }
 
         @Override
-        protected void onPostExecute(Boolean success) {
-            if (success) {
-                Toast.makeText(MainActivity.this, "Value saved to database", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(MainActivity.this, "Failed to save value to database", Toast.LENGTH_SHORT).show();
-            }
+        protected void onPostExecute(String result) {
+            Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
         }
     }
 
     private class LoadDataAsyncTask extends AsyncTask<Void, Void, String> {
         @Override
         protected String doInBackground(Void... params) {
-            String value = "";
+            String result = "";
 
             try {
-                Class.forName("com.mysql.jdbc.Driver");
-                Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-                PreparedStatement statement = connection.prepareStatement("SELECT name FROM your-table-name");
-                ResultSet resultSet = statement.executeQuery();
+                // 서버로부터 데이터를 불러오기 위한 GET 요청 전송
+                URL url = new URL(API_URL);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setRequestProperty("Content-Type", "application/json");
 
-                if (resultSet.next()) {
-                    value = resultSet.getString("name");
+                // 응답 읽기
+                InputStream inputStream = conn.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                StringBuilder response = new StringBuilder();
+                while ((line = bufferedReader.readLine()) != null) {
+                    response.append(line);
                 }
+                bufferedReader.close();
+                inputStream.close();
 
-                resultSet.close();
-                statement.close();
-                connection.close();
-            } catch (SQLException e) {
-                Log.e("LoadDataAsyncTask", "Error executing SQL query: " + e.getMessage());
+                // JSON 응답 파싱
+                JSONObject jsonResponse = new JSONObject(response.toString());
+                String name = jsonResponse.getString("name");
+
+                // 결과값 설정
+                result = "Name: " + name;
+
+                conn.disconnect();
+            } catch (JSONException | IOException e) {
                 e.printStackTrace();
-            } catch (ClassNotFoundException e) {
-                Log.e("LoadDataAsyncTask", "Error loading JDBC driver: " + e.getMessage());
-                e.printStackTrace();
+                result = "Error loading data: " + e.getMessage();
             }
 
-            return value;
+            return result;
         }
 
         @Override
-        protected void onPostExecute(String value) {
-            tvSql.setText(value);
-
+        protected void onPostExecute(String result) {
+            tvSql.setText(result);
         }
     }
 }
